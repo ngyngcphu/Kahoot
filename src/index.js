26c263873f41344355106d3ffa761e634174676c
ngyngcphu <nguyenngocphu225@gmail.com>
2023-01-04 09:51:21 +0700

    create quizform

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import QuizForm from './components/QuizForm';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <QuizForm />
   </React.StrictMode>
 );
 
